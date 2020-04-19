ca7bc33d1ecd955f4da753a7aa1eabf845910c02
Satiriques <bad_ghost2005@hotmail.com>
2020-04-19 14:39:19 -0400

    fixed failing unit tests

--- LaraxsBotTests/Services/Classes/NuitManagerServiceTests.cs
+++ LaraxsBotTests/Services/Classes/NuitManagerServiceTests.cs
@@ -1,17 +1,35 @@
 ﻿using LaraxsBot.Database.Contexts;
 using LaraxsBot.Services.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NCrunch.Framework;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace LaraxsBot.Services.Classes.Tests
 {
+    [ExclusivelyUses]
     [TestClass]
     [SuppressMessage("Design", "RCS1090:Call 'ConfigureAwait(false)'.", Justification = "<Pending>")]
     public class NuitManagerServiceTests
     {
-        private readonly INuitManagerService _service = new NuitManagerService(new NuitContext());
+        private NuitContext _nuitContext;
+        private INuitManagerService _service;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _nuitContext = new NuitContext();
+            _service = new NuitManagerService(_nuitContext);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _nuitContext.Database.EnsureDeleted();
+            Directory.Delete(Path.Combine(AppContext.BaseDirectory, "data"));
+        }
 
         [TestMethod]
         public async Task CreateNuitAsyncTest()
@@ -20,14 +38,14 @@ namespace LaraxsBot.Services.Classes.Tests
             await _service.CreateNuitAsync(DateTime.Now, DateTime.Now + TimeSpan.FromDays(7), 0);
 
             var count = await _service.GetNumberOfNuitAsync();
-            Assert.AreEqual(1, count);
+            Assert.AreEqual(2, count);
 
             await _service.StopNuitAsync();
             await _service.CreateNuitAsync(DateTime.Now, DateTime.Now + TimeSpan.FromDays(7), 0);
 
             count = await _service.GetNumberOfNuitAsync();
 
-            Assert.AreEqual(2, count);
+            Assert.AreEqual(3, count);
         }
     }
 }
