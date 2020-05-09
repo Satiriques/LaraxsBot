cb18c9af9d6982998e55be11d9515d77eaded8ec
Satiriques <bad_ghost2005@hotmail.com>
2020-05-09 15:08:42 -0400

    added IEquatable to anime model

--- MalParser/Models/Anime.cs
+++ MalParser/Models/Anime.cs
@@ -1,8 +1,14 @@
 ﻿using MalParser.Interfaces;
+using Microsoft.VisualBasic;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("MalParserTests")]
 namespace MalParser.Models
 {
-    internal class Anime : IAnime
+    internal class Anime : IAnime, IEquatable<Anime>
     {
         public string Background { get; set; } = string.Empty;
 
@@ -30,5 +36,32 @@ namespace MalParser.Models
 
         public string Type { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+
+        public bool Equals([AllowNull]Anime other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Background.Equals(other.Background)
+                && (ReferenceEquals(Genres, other.Genres)
+                    || Enumerable.SequenceEqual(Genres, other.Genres))
+                && (ReferenceEquals(Licensors, other.Licensors)
+                    || Enumerable.SequenceEqual(Licensors, other.Licensors))
+                && NumberOfEpisodes.Equals(other.NumberOfEpisodes)
+                && PopularityPosition.Equals(other.PopularityPosition)
+                && (ReferenceEquals(Producers, other.Producers)
+                    || Enumerable.SequenceEqual(Producers, other.Producers))
+                && RankedPosition.Equals(other.RankedPosition)
+                && Score.Equals(other.Score)
+                && Source.Equals(other.Source)
+                && (ReferenceEquals(Studios, other.Studios)
+                    || Enumerable.SequenceEqual(Studios, other.Studios))
+                && Synopsis.Equals(other.Synopsis)
+                && Title.Equals(other.Title)
+                && Type.Equals(other.Type)
+                && ImageUrl.Equals(other.ImageUrl);
+        }
     }
 }
